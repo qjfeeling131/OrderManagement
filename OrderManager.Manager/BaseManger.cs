c81626049496d51a9b59aae136b308a5c4528b9f
Randy <randy.lai@egoal-tech.com>
2015-05-31 16:35:41 +0800

    basemanger

---
+++ OrderManager.Manager/BaseManger.cs
@@ -0,0 +1,15 @@
+﻿using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManager.Manager
+{
+    public class BaseManger
+    {
+        public IDatabaseRepository DbContext { get; set; }
+        
+    }
+}
